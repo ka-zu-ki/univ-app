757ceb5b64fd0ddeebf659c1ca0f087684f59402
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-02-20 13:14:04 +0900

    lessonのrouteを設定  #7

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'user', controllers: {
         registrations: 'api/v1/auth/registrations'
-      }
+      } 
+      
+      resources :lessons
     end
   end
 end
