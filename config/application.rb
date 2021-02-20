0f1d0e42684c8c572c2c0128a4491b5ced1849fe
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-02-20 17:00:56 +0900

    fakerを日本語化 #7

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,7 @@ module UniversityApp
                  :methods => [:get, :post, :options, :delete, :put]
       end
     end
+    config.i18n.available_locales = %i[ja en]
+    config.i18n.default_locale = :ja
   end
 end
