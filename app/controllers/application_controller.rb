0d80a513f3eb81f5d0b0f93ae741ebc0828660ee
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-02-28 15:12:52 +0900

    ログイン、ログアウトのアクションを作成しリクエスト成功を確認  #25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,13 @@ class ApplicationController < ActionController::API
   def login!
     session[:user_id] = @user.id
   end
+
+  def current_user
+    @current_user || User.find(session[:user_id]) if session[:user_id]
+  end
   
   def slow_load
     sleep(1)
   end
-  
+
 end
