e6f1cf0f10f7b25ce962b6fc355094613bbd5853
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-02-28 11:32:52 +0900

    devise-token-auth関連を削除 #25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :slow_load
   
   def slow_load
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'user', controllers: {
-        registrations: 'api/v1/auth/registrations'
-      }
       
       resources :lessons do
         resources :myclasses
