36c806e490c40572a88f3e4ddc034c5c88d57139
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-03-04 16:43:20 +0900

    授業の時間割を判別できるようにした #29

--- app/controllers/api/v1/myclasses_controller.rb
+++ app/controllers/api/v1/myclasses_controller.rb
@@ -1,6 +1,30 @@
 class Api::V1::MyclassesController < ApplicationController
   def create
-    myclass = Myclass.new(lesson_id: params[:lesson_id], user_id: params[:user_id])
+    new_lesson = Lesson.find(params[:lesson_id])
+
+    now_lessons = Myclass.find_by(active: true)
+    same_time_lesson = now_lessons.class.where('week = ? and time = ?', new_lesson.week, new_lesson.time)
+    
+    if same_time_lesson
+      same_time_lesson.first.update_attribute(:active, false)
+
+      myclass = Myclass.new(
+        lesson_id: new_lesson.id,
+        user_id: params[:user_id],
+        week: new_lesson.week,
+        time: new_lesson.time,
+        active: true
+      )
+    else
+      myclass = Myclass.new(
+        lesson_id: new_lesson.id, 
+        user_id: params[:user_id],
+        week: new_lesson.week,
+        time: new_lesson.time,
+        active: true
+      )
+    end
+
     myclass.save!
     
     render json: myclass
--- app/models/myclass.rb
+++ app/models/myclass.rb
@@ -1,4 +1,6 @@
 class Myclass < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+
+  scope :same_time_lesson?, -> (now_lesson_id) {}
 end
