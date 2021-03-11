7b7f85dff778a0b4df5a26a77f6ecf08e2859997
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-03-11 20:04:35 +0900

    url末尾のスラッシュを削除 #51

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,8 @@ module UniversityApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://univ-app-front-id6tydqal-ka-zu-ki.vercel.app/'
         
+        origins "http://localhost:3001", "https://univ-app-front-9lmcr4mz3-ka-zu-ki.vercel.app"
         resource "*",
           headers: :any,
           methods: [:get, :post, :delete, :put, :show, :options, :head],
