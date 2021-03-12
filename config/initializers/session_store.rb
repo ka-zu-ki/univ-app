6e1021f47110aee1df50dd4671d720eb657845de
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-03-12 11:44:24 +0900

    ダミーデータを修正 #51

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_univ-app', domain: 'univ-app-front-git-deploy-ka-zu-ki.vercel.app'
+  Rails.application.config.session_store :cookie_store, key: '_univ-app-front', domain: 'univ-app-front-git-deploy-ka-zu-ki.vercel.app'
 else
   Rails.application.config.session_store :cookie_store, key: '_univ-app'
 end
