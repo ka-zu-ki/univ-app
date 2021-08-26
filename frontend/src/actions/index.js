f894f10652c631c9b3d068c4939e0dcb16bda408
kazuki <66470195+ka-zu-ki@users.noreply.github.com>
2021-08-26 13:50:44 +0900

    Delete frontend directory

--- frontend/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const SIGN_UP = "SIGN_UP"
-export const LOG_IN = "LOG_IN"
-export const LOGGED_IN = "LOGGED_IN"
-export const LOG_OUT = "LOG_OUT"
-
-export const FETCHING = 'FETCHING'
-export const FETCHING_SUCCESS = 'FETCHING_SUCCESS'
--- frontend/src/components/Style/Object/Utility/margin.js
+++
@@ -1,9 +0,0 @@
-import { css } from "@emotion/react";
-
-export const marginTop10Css = css`
-  margin-top: 10px;
-`
-
-export const marginTop30Css = css`
-  margin-top: 30px;
-`
