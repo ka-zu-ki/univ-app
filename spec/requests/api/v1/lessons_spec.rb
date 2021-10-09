a7f1c4b8c2723b77490214f73a36340657d08914
ka-zu-ki <yamakazuki0721@gmail.com>
2021-10-09 15:50:27 +0900

    create-spec

--- spec/requests/api/v1/lessons_spec.rb
+++ spec/requests/api/v1/lessons_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Lessons", type: :request do
   let!(:user) { create(:user) }
-  let!(:lessons) { create_list(:lesson, 3) }
+  let!(:lesson) { create_list(:lesson, 3) }
 
   describe "GET /api/v1/lessons" do
     it "200 Status" do
@@ -10,4 +10,10 @@ RSpec.describe "Api::V1::Lessons", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /api/v1/lessons/lesson_id"
+    it "200 Status" do
+      get api_v1_lessons_path(lesson.id)
+      expect(response).to have_http_status(200)
+    end
 end
